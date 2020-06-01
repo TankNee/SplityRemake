6028a9ec76a52fbb742d75e1e09df52cb1d35106
tanknee <nee@tanknee.cn>
2020-06-01 10:37:58 +0800

    replace the image placeholder

--- index-hd.php
+++ index-hd.php
@@ -17,7 +17,8 @@ for ($i = 0; $i < $n; $i++) {
     } else {
         $thumbnail = showThumbnail($ji, 0);
     }
-    $html = $html . '<div class="card item list-item list-overlay-content m-0"><div class="media media-21x9"><a class="media-content" href="' . $ji->permalink . '" style="background-image:url(' . $thumbnail . ')" ><span class="overlay"></span></a></div><div class="list-content p-3 p-md-5 text-center"><div class="list-body"><a href="' . $ji->permalink . '" target="_blank" class="h4 text-white h-2x m-0">' . $ji->title . '</a> </div></div></div>';
+    // $html = $html . '<div class="card item list-item list-overlay-content m-0"><div class="media media-21x9"><a class="media-content" href="' . $ji->permalink . '" style="background-image:url(' . $thumbnail . ')" ><span class="overlay"></span></a></div><div class="list-content p-3 p-md-5 text-center"><div class="list-body"><a href="' . $ji->permalink . '" target="_blank" class="h4 text-white h-2x m-0">' . $ji->title . '</a> </div></div></div>';
+    $html = $html . '<div class="card item list-item list-overlay-content m-0"><div class="media media-21x9"><a class="media-content" href="' . $ji->permalink . '" ><img class="lazy" data-original="' . $thumbnail  . '"><span class="overlay"></span></a></div><div class="list-content p-3 p-md-5 text-center"><div class="list-body"><a href="' . $ji->permalink . '" target="_blank" class="h4 text-white h-2x m-0">' . $ji->title . '</a> </div></div></div>';
 }
 echo $html;
 ?>
